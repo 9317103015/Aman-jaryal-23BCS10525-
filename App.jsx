4bb010aa9882402e01e97d6d6132e6a5ceb44b55
Aman Jaryal <23BCS10525@cuchd.in>
2025-10-04 14:40:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>This is some app content</p>
+    </div>
+  );
+};
+
+export default App;
